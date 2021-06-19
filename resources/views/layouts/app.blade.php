bba2aae0cfe6de5a79438cddc6d790c286f5e4cd
olakehinde <olakehinde59@gmail.com>
2021-06-19 20:05:25 +0100

    added default app nav bar

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posty</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between">
+        <ul class="flex items-center">
+            <li> 
+                <a href="" class="p-3">Home</a>
+            </li> 
+            <li> 
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li> 
+                <a href="" class="p-3">Posts</a>
+            </li>
+        </ul>
+
+        <ul class="flex items-center">
+            <li> 
+                <a href="" class="p-3">Olamide Kehinde </a>
+            </li> 
+            <li> 
+                <a href="" class="p-3">Login</a>
+            </li>
+            <li> 
+                <a href="" class="p-3">Register</a>
+            </li>
+            <li> 
+                <a href="" class="p-3">Logout</a>
+            </li>
+        </ul>
+
+    </nav>
+
+
+    @yield('content')
+</body>
+</html>
