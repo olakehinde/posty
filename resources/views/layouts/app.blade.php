4a8027c06c42db36175778de0b0902482f0dc3d6
olakehinde <olakehinde59@gmail.com>
2021-06-20 06:02:38 +0100

    added auth and guest control in the app navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,27 +12,35 @@
             <li> 
                 <a href="" class="p-3">Home</a>
             </li> 
-            <li> 
-                <a href="" class="p-3">Dashboard</a>
-            </li>
-            <li> 
-                <a href="" class="p-3">Posts</a>
-            </li>
+
+            @auth
+                <li> 
+                    <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
+                </li>
+                <li> 
+                    <a href="" class="p-3">Posts</a>
+                </li>
+            @endauth
         </ul>
 
         <ul class="flex items-center">
+        @auth
             <li> 
                 <a href="" class="p-3">Olamide Kehinde </a>
             </li> 
             <li> 
-                <a href="" class="p-3">Login</a>
-            </li>
-            <li> 
-                <a href="" class="p-3">Register</a>
-            </li>
-            <li> 
                 <a href="" class="p-3">Logout</a>
             </li>
+        @endauth
+
+        @guest
+            <li> 
+                <a href="{{ route('register') }}" class="p-3">Register</a>
+            </li>
+            <li> 
+                <a href="" class="p-3">Login</a>
+            </li>
+        @endguest
         </ul>
 
     </nav>
