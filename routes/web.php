337a7b1efbd6964937a9dac21004d2f7fb9bf120
olakehinde <olakehinde59@gmail.com>
2021-06-20 06:01:59 +0100

    added dashboard and more register route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\RegisterController;
 
 /*
@@ -13,7 +14,9 @@ use App\Http\Controllers\Auth\RegisterController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/register', [RegisterController::class, 'create'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/posts', function () {
     return view('posts.index');
 });
