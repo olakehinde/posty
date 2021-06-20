d0a4381049ff943ac2098e332fa7097ca9fc0c58
olakehinde <olakehinde59@gmail.com>
2021-06-20 06:37:33 +0100

    added login controller

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index() {
+        return view('auth.login');
+    }
+
+    public function login(Request $request) {
+        //validate user credentials
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // sign user in
+        if(!auth()->attempt($request->only('email', 'password'))) {
+            return back()->with('status', 'Invalid Login credentials');
+        }
+
+        //redirect to dashboard page
+        return redirect()->route('dashboard');
+    }
+}
