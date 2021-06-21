83cc4de1a4444adc5854f195d66d73da25455bcf
olakehinde <olakehinde59@gmail.com>
2021-06-21 16:42:49 +0100

    created post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('posts.index');
+        // dd(Post::get());
+        $posts = Post::paginate(2);
+
+        return view('posts.index', [
+            'posts' => $posts,
+        ]);
     }
 
     public function store(Request $request) {
-        dd('okay');
+        $this->validate($request, [
+            'body' => 'required',
+        ]);
+
+        $request->user()->posts()->create([
+            'body' => $request->body
+        ]);
+       // $request->user()->posts()->create($request->only('body'));
+
+        return back();
     }
 }
