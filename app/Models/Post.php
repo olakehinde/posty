6a6dc2297909f2e3d9a4c20be131e0d87fa1c871
olakehinde <olakehinde59@gmail.com>
2021-06-21 16:42:30 +0100

    added eloquent relationship with user model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\User;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +9,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'body',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
