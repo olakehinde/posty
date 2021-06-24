8cadc4aab7634245d7f0964f52f1daaea5bebfca
olakehinde <olakehinde59@gmail.com>
2021-06-24 20:55:23 +0100

    added the delete form into the post page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -25,17 +25,28 @@
                 @foreach ($posts as $post)
                     <div class="mb-4">
                         <a href="" class="font-bold">{{ $post->user->name }}</a> <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
+                        
                         <p class="mb-2">{{ $post->body }}</p>  
+
+                        @if ($post->ownedBy(auth()->user()))
+                            <div>
+                                <form action="{{ route('posts.delete', $post) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="text-blue-500">Delete</button>
+                                </form>
+                            </div>
+                        @endif
 
                         <div class="flex items-center">
                             @auth
                                 @if (!$post->likedBy(auth()->user()))
-                                    <form action="{{ route('posts.like', $post->id) }}" method="post" class="mr-1">
+                                    <form action="{{ route('posts.like', $post) }}" method="post" class="mr-1">
                                         @csrf
                                         <button type="submit" class="text-blue-500">Like</button>
                                     </form>
                                 @else
-                                    <form action="{{ route('posts.like', $post->id) }}" method="post" class="mr-1">
+                                    <form action="{{ route('posts.like', $post) }}" method="post" class="mr-1">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="text-blue-500">Unlike</button>
